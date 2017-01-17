10d19e55b60fcc8f93eb73124a33ce7f97ec4f25
kristdevelop <kyllirist@gmail.com>
2017-01-17 12:29:58 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * Decompiled with CFR 0_118.
+ * 
+ * Could not load the following classes:
+ *  javafx.application.Application
+ *  javafx.stage.Stage
+ */
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main
+extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        new Lauamang();
+    }
+}
+
